750fc83211c9d673cefb1fd7ad2811f86f5593f0
Krishna Kumar <dev@krishnaaa.com>
2025-06-12 08:55:16 +0530

    init: turbo repo

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: false,
+  trailingComma: "all",
+};
+
+export default config;
